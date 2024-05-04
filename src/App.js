aac7f7d3e900f7e8e983b730ac80e69b12232503
EduardEgcho <egiazaryan-eduard@example.com>
2024-05-04 18:22:58 +0300

    hdg

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import PostPage from "./pages/PostPage/postPage";
+
+function App() {
+
+    return (
+        <div>
+            <h1>Итоговое задание</h1>
+            <PostPage/>
+        </div>
+    )
+
+}
+
+export default App;
+
+
+
+
+
+
